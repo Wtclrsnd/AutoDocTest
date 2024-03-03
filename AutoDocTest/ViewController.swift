b79d2e861666685ac76c0e3e396d25037749b872
WhiteColorSand <e.imamov@cappasity.com>
2024-03-03 19:20:46 +0300

    Initial Commit

---
+++ AutoDocTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoDocTest
+//
+//  Created by Emil Shpeklord on 03.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
