4662c5d082addc7938296bc141d5d5de73dc567d
WhiteColorSand <e.imamov@cappasity.com>
2024-03-03 19:41:38 +0300

    removed storyboard

--- AutoDocTest/MainScene/NewsViewController.swift
+++ AutoDocTest/MainScene/NewsViewController.swift
@@ -7,10 +7,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class NewsViewController: UIViewController {
 
+    private let viewModel: NewsViewModel
+    
+    init(vm: NewsViewModel) {
+        self.viewModel = vm
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .red
         // Do any additional setup after loading the view.
     }
 
